e2e67f6cc8957fa64baf4e91b148b8f185fdc00c
hekki <contact@hekki.info>
2019-11-05 01:35:17 +0900

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(page_title)
+  def full_title(page_title = nil)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.nil?
       base_title
